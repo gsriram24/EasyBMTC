db46a9cc20f8600ed79635afe1df2b5b7231249b
G Sriram <sgcuber24@gmail.com>
2019-10-07 13:03:47 +0530

    Adding pickers

--- App/Components/BusDetails/index.js
+++ App/Components/BusDetails/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View, ActivityIndicator, Alert } from 'react-native';
+import { Text, View, ActivityIndicator, Alert, StatusBar } from 'react-native';
+import styles from './style.js';
+import ModalSelector from 'react-native-modal-selector';
+import CardView from 'react-native-cardview';
 
 export default class App extends Component {
 	constructor(props) {
@@ -8,7 +11,11 @@ export default class App extends Component {
 	}
 
 	componentDidMount() {
-		return fetch('https://easybmtc.herokuapp.com/api/v1/busses/' + this.props.navigation.state.params.number)
+		StatusBar.setBarStyle('light-content', true);
+		StatusBar.setBackgroundColor('#50c96a');
+		return fetch(
+			'https://easybmtc.herokuapp.com/api/v1/busses/' + /*this.props.navigation.state.params.number*/ 'KA01F4340'
+		)
 			.then((response) => response.json())
 			.then((responseJson) => {
 				this.setState({
@@ -23,15 +30,61 @@ export default class App extends Component {
 	render() {
 		if (this.state.isLoading === true) {
 			return (
-				<View>
-					<ActivityIndicator size="large" />
+				<View style={styles.body}>
+					<ActivityIndicator color="#50c96a" size="large" style={styles.activityIndicator} />
 				</View>
 			);
 		}
-
+		const stopsData = this.state.details.busStops.map((item, index) => {
+			return { key: index, label: item };
+		});
+		console.log(stopsData);
 		return (
-			<View>
-				<Text> {this.state.details.busReg} </Text>
+			<View style={styles.body}>
+				<View style={styles.headerContainer}>
+					<Text style={styles.headerText}>Journey Details</Text>
+					<Text style={styles.headerText}>{this.state.details.busNo}</Text>
+				</View>
+				<View style={styles.bodyContainer}>
+					<CardView cardElevation={4} cardMaxElevation={7} cornerRadius={7} style={styles.card}>
+						<View style={styles.pickerBox}>
+							<Text style={{ color: '#bababa' }}>From</Text>
+							<ModalSelector
+								selectStyle={{ borderWidth: 0 }}
+								initValueTextStyle={{ color: '#575757' }}
+								selectTextStyle={{ color: '#575757' }}
+								style={styles.pickerFrom}
+								data={stopsData}
+								initValue="From"
+								onChange={(option) => {
+									this.setState({ from: option.key });
+								}}
+								cancelText="Cancel"
+							/>
+						</View>
+						<View
+							style={{
+								borderBottomColor: '#cacaca',
+								borderBottomWidth: 1
+							}}
+						/>
+						<View style={styles.pickerBox}>
+							<Text style={{ color: '#bababa' }}>To</Text>
+							<ModalSelector
+								selectStyle={{ borderWidth: 0 }}
+								initValueTextStyle={{ color: '#575757' }}
+								selectTextStyle={{ color: '#575757' }}
+								style={styles.pickerFrom}
+								data={stopsData}
+								initValue="To"
+								onChange={(option) => {
+									this.setState({ to: option.key });
+								}}
+								cancelText="Cancel"
+							/>
+						</View>
+					</CardView>
+				</View>
 			</View>
 		);
 	}
---
+++ App/Components/BusDetails/style.js
@@ -0,0 +1,50 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	body: {
+		flex: 1
+	},
+	activityIndicator: {
+		transform: [ { scale: 2 } ],
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 80
+	},
+	headerContainer: {
+		flex: 1.25,
+		backgroundColor: '#66db7f',
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 8
+		},
+		shadowOpacity: 0.46,
+		shadowRadius: 11.14,
+
+		elevation: 7
+	},
+	headerText: {
+		marginTop: 15,
+		color: '#ffffff',
+		fontSize: 16,
+		fontWeight: 'bold',
+		alignSelf: 'center'
+	},
+	bodyContainer: {
+		flex: 8.75,
+		backgroundColor: '#eaeaea'
+	},
+	pickerBox: {
+		marginTop: 10,
+		marginBottom: 10,
+		width: 300,
+		alignSelf: 'center'
+	},
+
+	card: {
+		marginTop: 40,
+		width: 350,
+		alignSelf: 'center'
+	}
+});
