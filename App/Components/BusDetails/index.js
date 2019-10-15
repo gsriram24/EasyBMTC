bbc5cf6c2f04878ca665763f641f836df86ea01f
G Sriram <sgcuber24@gmail.com>
2019-10-15 09:16:59 +0530

    Added fare calculation

--- App/Components/BusDetails/index.js
+++ App/Components/BusDetails/index.js
@@ -5,19 +5,24 @@ import ModalSelector from 'react-native-modal-selector';
 import CardView from 'react-native-cardview';
 import NumericInput from 'react-native-numeric-input';
 import LinearGradient from 'react-native-linear-gradient';
-
 export default class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { isLoading: true };
+		this.state = {
+			isLoading: true,
+			from: 0,
+			to: 0,
+			numberAdults: 0,
+			numberChildren: 0,
+			numberSC: 0,
+			fare: 0
+		};
 	}
 
 	componentDidMount() {
 		StatusBar.setBarStyle('light-content', true);
 		StatusBar.setBackgroundColor('#50c96a');
-		return fetch(
-			'https://easybmtc.herokuapp.com/api/v1/busses/' + /*this.props.navigation.state.params.number*/ 'KA01F4340'
-		)
+		return fetch('https://easybmtc.herokuapp.com/api/v1/busses/' + this.props.navigation.state.params.number)
 			.then((response) => response.json())
 			.then((responseJson) => {
 				this.setState({
@@ -29,6 +34,229 @@ export default class App extends Component {
 				console.error(error);
 			});
 	}
+	calculateFare = () => {
+		const adult = [
+			0,
+			5,
+			9,
+			12,
+			15,
+			16,
+			16,
+			17,
+			18,
+			20,
+			20,
+			20,
+			21,
+			21,
+			21,
+			21,
+			22,
+			22,
+			22,
+			23,
+			23,
+			23,
+			24,
+			24,
+			24,
+			26,
+			26,
+			26,
+			27,
+			27,
+			27,
+			28,
+			28,
+			28,
+			29,
+			29,
+			29,
+			30,
+			30,
+			30,
+			31,
+			31,
+			31,
+			32,
+			32,
+			32,
+			33,
+			33,
+			33,
+			34,
+			34,
+			34,
+			35,
+			35,
+			35,
+			36,
+			36,
+			36,
+			37,
+			37,
+			37,
+			38,
+			38,
+			38,
+			39,
+			39,
+			39
+		];
+		const child = [
+			0,
+			3,
+			5,
+			6,
+			8,
+			8,
+			8,
+			9,
+			9,
+			10,
+			10,
+			10,
+			11,
+			11,
+			11,
+			11,
+			11,
+			11,
+			11,
+			12,
+			12,
+			12,
+			12,
+			12,
+			12,
+			13,
+			13,
+			13,
+			14,
+			14,
+			14,
+			14,
+			14,
+			14,
+			15,
+			15,
+			15,
+			15,
+			15,
+			15,
+			16,
+			16,
+			16,
+			16,
+			16,
+			16,
+			17,
+			17,
+			17,
+			17,
+			17,
+			17,
+			18,
+			18,
+			18,
+			18,
+			18,
+			18,
+			19,
+			19,
+			19,
+			19,
+			19,
+			19,
+			20,
+			20,
+			20
+		];
+		const sc = [
+			0,
+			4,
+			7,
+			9,
+			11,
+			12,
+			12,
+			13,
+			14,
+			15,
+			15,
+			15,
+			16,
+			16,
+			16,
+			16,
+			17,
+			17,
+			17,
+			17,
+			17,
+			17,
+			18,
+			18,
+			18,
+			20,
+			20,
+			20,
+			20,
+			20,
+			20,
+			21,
+			21,
+			21,
+			22,
+			22,
+			22,
+			23,
+			23,
+			23,
+			23,
+			23,
+			23,
+			24,
+			24,
+			24,
+			25,
+			25,
+			25,
+			26,
+			26,
+			26,
+			26,
+			26,
+			26,
+			27,
+			27,
+			27,
+			28,
+			28,
+			28,
+			29,
+			29,
+			29,
+			29,
+			29,
+			29
+		];
+		let fromIndex = this.state.from;
+		let toIndex = this.state.to;
+		let indexDiff = Math.abs(fromIndex - toIndex);
+		if (indexDiff == 0) {
+			return 0;
+		}
+		let cost =
+			adult[parseInt(indexDiff / 2) + 1] * this.state.numberAdults +
+			child[parseInt(indexDiff / 2) + 1] * this.state.numberChildren +
+			sc[parseInt(indexDiff / 2) + 1] * this.state.numberSC;
+		return cost;
+	};
+	goBack = () => {
+		this.props.navigation.navigate('QRScreen');
+	};
 	render() {
 		if (this.state.isLoading === true) {
 			return (
@@ -91,7 +319,7 @@ export default class App extends Component {
 							<Text style={{ color: '#bababa' }}>Adults</Text>
 							<NumericInput
 								type="up-down"
-								onChange={(value) => console.log(value)}
+								onChange={(value) => this.setState({ numberAdults: value })}
 								rounded
 								containerStyle={{ alignSelf: 'center' }}
 								minValue={0}
@@ -108,7 +336,7 @@ export default class App extends Component {
 							<Text style={{ color: '#bababa' }}>Children</Text>
 							<NumericInput
 								type="up-down"
-								onChange={(value) => console.log(value)}
+								onChange={(value) => this.setState({ numberChildren: value })}
 								rounded
 								containerStyle={{ alignSelf: 'center' }}
 								minValue={0}
@@ -125,7 +353,7 @@ export default class App extends Component {
 							<Text style={{ color: '#bababa' }}>Senior Citizens</Text>
 							<NumericInput
 								type="up-down"
-								onChange={(value) => console.log(value)}
+								onChange={(value) => this.setState({ numberSC: value })}
 								rounded
 								containerStyle={{ alignSelf: 'center' }}
 								inputStyle={{ color: '#575757' }}
@@ -136,7 +364,9 @@ export default class App extends Component {
 				</View>
 				<View style={styles.footerContainer}>
 					<TouchableOpacity style={styles.backButton}>
-						<Text style={styles.backText}>Back</Text>
+						<Text style={styles.backText} onPress={this.goBack}>
+							Back
+						</Text>
 					</TouchableOpacity>
 					<LinearGradient
 						colors={[ '#50c96a', '#77e98f' ]}
@@ -145,7 +375,10 @@ export default class App extends Component {
 						style={styles.payButton}
 					>
 						<TouchableOpacity>
-							<Text style={styles.payText}>Pay {'\u20B9'}12</Text>
+							<Text style={styles.payText}>
+								Pay {'\u20B9'}
+								{this.calculateFare()}
+							</Text>
 						</TouchableOpacity>
 					</LinearGradient>
 				</View>
--- App/Components/Navigators/QRSwitchNavigator.js
+++ App/Components/Navigators/QRSwitchNavigator.js
@@ -3,14 +3,14 @@ import BusTicketSwitchNavigator from '../Navigators/BusTicketSwitchNavigator';
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 
 const AppNavigator = createSwitchNavigator({
-	BusTicketSwitchNavigator: {
-		screen: BusTicketSwitchNavigator,
+	QRScreen: {
+		screen: QRScreen,
 		navigationOptions: {
 			header: null
 		}
 	},
-	QRScreen: {
-		screen: QRScreen,
+	BusTicketSwitchNavigator: {
+		screen: BusTicketSwitchNavigator,
 		navigationOptions: {
 			header: null
 		}
